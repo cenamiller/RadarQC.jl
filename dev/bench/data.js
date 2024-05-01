1df0546067923c167e8e2f577dbef493e8e73138
github-action-benchmark <github@users.noreply.github.com>
2024-05-01 15:33:14 +0000

    add Julia benchmark result (julia) benchmark result for 8bd42f7660b99291f8619d6ec1bf06bd654a89f9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714577115303,
+  "lastUpdate": 1714577594678,
   "repoUrl": "https://github.com/cenamiller/RadarQC.jl",
   "entries": {
     "Julia benchmark result": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 2019446640,
             "unit": "ns",
             "extra": "gctime=128831674\nmemory=2805573960\nallocs=17716686\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cena.miller@gmail.com",
+            "name": "cenamiller",
+            "username": "cenamiller"
+          },
+          "committer": {
+            "email": "cena.miller@gmail.com",
+            "name": "cenamiller",
+            "username": "cenamiller"
+          },
+          "distinct": true,
+          "id": "8bd42f7660b99291f8619d6ec1bf06bd654a89f9",
+          "message": "fixed markdown in Derecho section of README. Added jl script version of example notebook that was run on Derecho.",
+          "timestamp": "2024-05-01T09:26:13-06:00",
+          "tree_id": "98dc1d566eef2349eb05a0d4d0e07f8c7fe68a6e",
+          "url": "https://github.com/cenamiller/RadarQC.jl/commit/8bd42f7660b99291f8619d6ec1bf06bd654a89f9"
+        },
+        "date": 1714577594212,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "features/10",
+            "value": 2080997124,
+            "unit": "ns",
+            "extra": "gctime=142526608\nmemory=2805573960\nallocs=17716686\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
